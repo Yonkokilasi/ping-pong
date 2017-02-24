24ca6cefbc974d46b47cd9f40a29d174695e7d71
yonkokilasi <yonkokilasi@gmail.com>
2017-02-24 16:25:13 +0300

    finals

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//back end
+function convert(number) {
+    var arrayinput = [];
+    for (var i = 1; i <= number; i++) {
+        if (i % 15 === 0) {
+            arrayinput.push("pingpong");
+        } else if (i % 3 === 0) {
+            arrayinput.push("ping");
+        } else if (i % 5 === 0) {
+            arrayinput.push("pong");
+        } else {
+            arrayinput.push(i);
+        }
+    }
+    return arrayinput;
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+//front-end
+$(document).ready(function() {
+    $(".blanks form").submit(function(event) {
+        var user = parseInt($("input#entry").val());
+        $("input#entry").val(" ");
+        var output = convert(user);
+        output.forEach(function(element) {
+            $("#result").append("<li>" + element + "</li>")
+            event.preventDefault();
+        });
+    });
+});
